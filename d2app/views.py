54696af9f0431cf13fcb1e0393f866309830f5f4
MontagueM <montaguemoran01@gmail.com>
2020-08-09 17:12:04 +0100

    Fixed small issues with formatting

--- d2app/views.py
+++ d2app/views.py
@@ -30,12 +30,13 @@ def get_buttons_html():
         model_data_file = model_unpacker.get_model_data_file(model_files[i])
         if model_data_file.split('-')[0] != folder.split('_')[-1]:
             continue
-        new_html += f'' \
-                    f'<div class="col-3">' \
+        new_html += f'<div class="col-4" id="button">' \
+                    f'<form action="submit" method="post">' \
                     f'<button class="btn btn-outline-dark" type="submit" name="{model_files[i]}">{model_data_file}</button>' \
+                    f'</form>' \
                     f'</div>' \
-                    f'<div class="col-5">' \
-                    f'<input type="text" name="model-desc" placeholder="Model desc."/>' \
+                    f'<div class="col-8 my-auto" id="model-desc">' \
+                    f'<input type="text" name="model-desc" placeholder="Model desc." value="">' \
                     f'</div>' \
                     f'\n'
 
@@ -57,21 +58,17 @@ def index(request):
 
 
 def submit(request):
-    """Functioned invoked by the user when they submit which song they prefer.
-    Uses the assigned name of the button to determine which song they chose, and
-    returns the ids of both songs involved"""
+    """
+    We're ignoring the model desc. part of the form since we cannot work with it until we have javascript-only.
+    """
     try:
         info = request.POST
-        print(f"Info gotten from input is {info.get('model-desc')}")
         # Extracts the song IDs from the "name" values of the button that was pressed
         model_file_name = list(info.items())[-1][0]
         model_file_hash = model_unpacker.get_hash_from_file(model_file_name).upper()
         print(f'Info: {model_file_name} {model_file_hash}')
         model_unpacker.get_model(model_unpacker.get_flipped_hex(model_file_hash, 8))
-        # Model has been sent to d2app/unpacked_objects/model.obj
         os.system('obj2gltf -i static/d2app/models/model.obj -o static/d2app/models/model.glb')
-
-
     except Exception as e:
         print(e)
 
